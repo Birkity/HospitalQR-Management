19f9e223545ec0abbb7b735f5f65f91a1cfac187
Birkity <birkity.yishak.m@gmail.com>
2025-02-01 15:28:46 +0300

    update auth : minor edits

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,10 +1,12 @@
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo
 from . import auth
+from ..models import User
 
 class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
@@ -19,7 +21,8 @@ class LoginForm(FlaskForm):
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        # Here you would typically store the user in the database
+        user = User(form.username.data, form.email.data, form.password.data, "patient")
+        user.save_to_db()
         flash('Registration successful!', 'success')
         return redirect(url_for('auth.login'))
     return render_template('register.html', form=form)
@@ -28,7 +31,9 @@ def register():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        # Here you would typically check credentials against a database
-        flash('Login successful!', 'success')
-        return redirect(url_for('main.index'))
+        user = User.find_by_email(form.email.data)
+        if user and User.verify_password(user['password'], form.password.data):
+            flash('Login successful!', 'success')
+            return redirect(url_for('main.index'))
+        flash('Login unsuccessful. Please check email and password.', 'danger')
     return render_template('login.html', title='Login', form=form)
