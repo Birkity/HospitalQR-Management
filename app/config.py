5cb8422bf1a3d187e42fbffb4ecb9d43d4467539
Birkity <birkity.yishak.m@gmail.com>
2025-02-01 13:51:09 +0300

    Update config py

--- app/config.py
+++ app/config.py
@@ -1,4 +1,10 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
+    SECRET_KEY = os.getenv('SECRET_KEY', 'your-secret-key')
+    MONGODB_URI = os.getenv('MONGODB_URI', 'mongodb://localhost:27017/hospital_db')
+    CHAPA_PUBLIC_KEY = os.getenv('CHAPA_PUBLIC_KEY')
+    CHAPA_SECRET_KEY = os.getenv('CHAPA_SECRET_KEY')
