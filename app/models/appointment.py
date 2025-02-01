1d958353adeeef22aa59328fab9ea07e795dc3ec
Birkity <birkity.yishak.m@gmail.com>
2025-02-01 15:02:41 +0300

    Add appointment model for db

---
+++ app/models/appointment.py
@@ -0,0 +1,23 @@
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
+from ..config import Config
+
+client = MongoClient(Config.MONGODB_URI)
+db = client.hospital_db
+
+class Appointment:
+    def __init__(self, patient_id, doctor_id, appointment_date, appointment_time, status, notes=""):
+        self.patient_id = patient_id
+        self.doctor_id = doctor_id
+        self.appointment_date = appointment_date
+        self.appointment_time = appointment_time
+        self.status = status
+        self.notes = notes
+    
+    def save_to_db(self):
+        return db.appointments.insert_one(self.__dict__).inserted_id
+
+    @staticmethod
+    def find_by_id(appointment_id):
+        return db.appointments.find_one({"_id": ObjectId(appointment_id)})
